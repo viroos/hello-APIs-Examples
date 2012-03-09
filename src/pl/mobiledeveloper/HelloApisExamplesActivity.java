7d1f4ebe6d12d4f81b70eebb3dc0632b6ae31beb
Maciej Sawicki <maciej.sawicki@polidea.pl>
2012-03-09 01:29:03 +0100

    -added missing files

--- src/pl/mobiledeveloper/HelloApisExamplesActivity.java
+++ src/pl/mobiledeveloper/HelloApisExamplesActivity.java
@@ -1,13 +1,52 @@
 package pl.mobiledeveloper;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import android.app.Activity;
+import android.content.res.Resources;
+import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HelloApisExamplesActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	URI uri = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Resources res = getResources();
+		try {
+			uri = new URI(res.getString(R.string.URI));
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		setContentView(R.layout.main);
+	}
+
+	public void httpClientOnClick(View v) {
+		ExampleAsyncTask asyncTask = new ExampleAsyncTask(
+				new HttpClientDownloader(), this);
+		asyncTask.execute(uri);
+	}
+
+	public void httpURLConnectionOnClick(View v) {
+		ExampleAsyncTask asyncTask = new ExampleAsyncTask(
+				new HttpUrlConnectionDownloader(), this);
+		asyncTask.execute(uri);
+	}
+	
+	public void clearImage(View v){
+        ImageView image = (ImageView) findViewById(R.id.xkcdImageView);
+	    image.setImageDrawable(null);
+	}
 }
